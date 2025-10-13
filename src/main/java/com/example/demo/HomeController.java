9b218b7ea4ecfa64dce162ff400572cea8c67c80
brobrown-coder <deon102@naver.com>
2025-10-13 17:37:59 +0900

    spring study

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,17 +8,37 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String loginPage() {
-		return "login"; // resolves to templates/login.html via Thymeleaf
+		return "index"; // resolves to templates/login.html via Thymeleaf
 	}
 
-	@GetMapping("/main")
+	@GetMapping("/login")
 	public String mainPage() {
-		return "main"; // resolves to templates/main.html
+		return "auth/login"; // resolves to templates/main.html
 	}
 
 	@GetMapping("/cal")
 	public String calPage() {
-		return "cal"; // resolves to templates/cal.html
+		return "contents/calculator/cal"; // resolves to templates/cal.html
+	}
+
+	@GetMapping("/contents/calculator/plus")
+	public String plusPage() {
+		return "contents/calculator/plus"; // resolves to templates/cal.html
+	}
+
+	@GetMapping("/contents/calculator/minus")
+	public String minusPage() {
+		return "contents/calculator/minus"; // resolves to templates/cal.html
+	}
+
+	@GetMapping("/contents/calculator/gob")
+	public String gobPage() {
+		return "contents/calculator/gob"; // resolves to templates/cal.html
+	}
+
+	@GetMapping("/contents/calculator/nanum")
+	public String nanumPage() {
+		return "contents/calculator/nanum"; // resolves to templates/cal.html
 	}
 }
 
